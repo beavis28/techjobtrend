12b0a98cbc1a581c5a2a0257d1b8ffd21a4aa27a
satoshi <satoshiho2012@gmail.com>
2017-10-25 13:04:33 +0800

    updated region data

--- dataProcess/region_data.py
+++ dataProcess/region_data.py
@@ -8,7 +8,7 @@ output_data = []
 location_list = []
 job_count = 0
 
-latest_file_name = "jobs" + arrow.now().format('YYYY-MM-22') + ".json"
+latest_file_name = "jobs" + arrow.now().format('YYYY-MM-DD') + ".json"
 
 with open(join('processed', latest_file_name), 'r') as f:
     original_data = json.load(f)
@@ -31,7 +31,9 @@ with open(join('processed', latest_file_name), 'r') as f:
 for l in location_list:
     m = dict(Counter(l['tags']))
     n = sorted(m.items(), key=lambda x: (-x[1], x[0]))
-    output_data.append({l['location']: n})
+    o = n[:5]
+    p = l['location'].replace("- \r\n", "")
+    output_data.append({p: o})
 
 
 with open(join('result', 'region_data.json'), 'w') as outfile:
