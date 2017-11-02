44f3d9c2294b2e91ea17d337490a5d9f0dbecbea
satoshi <satoshiho2012@gmail.com>
2017-11-02 07:40:20 +0000

    update

--- dataProcess/amazon.py
+++ dataProcess/amazon.py
@@ -9,23 +9,39 @@ SECRET_ACCESS_KEY = "qpiqHlouLlzfNetgOy4ErN6bNazekOAZe15MAd1s"
 ASSOCIATE_TAG = "beavis2809-20"
  
 amazon = bottlenose.Amazon(ACCESS_KEY, SECRET_ACCESS_KEY, ASSOCIATE_TAG)
-response = amazon.ItemSearch(Keywords="javascript", SearchIndex="KindleStore", ResponseGroup="Images, ItemAttributes, Offers")
-soup = BeautifulSoup(response,"lxml")
+response1 = amazon.ItemSearch(Keywords="javascript", SearchIndex="KindleStore", ResponseGroup="Images, ItemAttributes, Offers")
+soup1 = BeautifulSoup(response1,"lxml")
+
+response2 = amazon.ItemSearch(Keywords="java", SearchIndex="KindleStore", ResponseGroup="Images, ItemAttributes, Offers")
+soup2 = BeautifulSoup(response1,"lxml")
+
+response3 = amazon.ItemSearch(Keywords="tensorflow", SearchIndex="KindleStore", ResponseGroup="Images, ItemAttributes, Offers")
+soup3 = BeautifulSoup(response1,"lxml")
+
 output_data = []
 
+
 #print(soup.find_all('item')[0]('itemlinks').prettify())
-for item in soup.find_all('item'):
+#for item in soup1.find_all('item'):
+index = 0
+for item in soup1.find_all('item'):  
+  index += 1
   output_data.append({'title': item.title.text, 'image': item.mediumimage.url.text, 'url': item.url.text})
+  if index == 3:
+    break;
+
+for item in soup2.find_all('item'):
+  index += 1
+  output_data.append({'title': item.title.text, 'image': item.mediumimage.url.text, 'url': item.url.text})
+  if index == 2:
+    break;
+
+for item in soup3.find_all('item'):
+  index += 1
+  output_data.append({'title': item.title.text, 'image': item.mediumimage.url.text, 'url': item.url.text})
+  if index == 2:
+    break;
 
 with open(join('result', 'amazon_data.json'), 'w') as outfile:
     json.dump(output_data, outfile)
 
-
-response = amazon.ItemSearch(Keywords="reactjs", SearchIndex="KindleStore", ResponseGroup="Images, ItemAttributes, Offers")
-soup = BeautifulSoup(response,"lxml")
-output_data = []
-for item in soup.find_all('item'):
-  output_data.append({'title': item.title.text, 'image': item.mediumimage.url.text, 'url': item.url.text})
-
-with open(join('result', 'high_performance_amazon_data.json'), 'w') as outfile:
-    json.dump(output_data, outfile)
