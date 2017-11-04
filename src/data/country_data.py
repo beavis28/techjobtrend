76aaa6fa6222a66d4919d124f4b1fbf0a77ab2a9
satoshi <satoshiho2012@gmail.com>
2017-11-04 21:23:04 +0000

    update

---
+++ src/data/country_data.py
@@ -0,0 +1,45 @@
+from os.path import isfile, join
+import json
+import arrow
+from collections import Counter
+
+output_data = []
+location_list = []
+
+latest_file_name = "jobs" + arrow.now().format('YYYY-MM-DD') + ".json"
+
+with open(join('processed', latest_file_name), 'r') as f:
+    original_data = json.load(f)
+
+    for x in original_data:
+        location = ""
+        if x['location']:
+            location = x['location'].split(",")[-1].replace("- \r\n", "").strip()
+        else:
+            continue
+        if not location_list:
+            location_list.append({'location': location, 'tags': x['tags']})
+        else:
+            bfind = False
+            for y in location_list:
+
+                if y['location'].lower() == location.lower():
+                    bfind = True
+                    y['tags'].extend(x['tags'])
+                    break
+
+            if bfind == False:
+                location_list.append({'location': location, 'tags': x['tags']})
+
+for l in location_list:
+    m = dict(Counter(l['tags']))
+    n = sorted(m.items(), key=lambda x: (-x[1], x[0]))
+    o = n[:5]
+    p = l['location'].replace("- \r\n", "")
+    output_data.append({p: o})
+
+
+output_data = sorted(output_data)
+
+with open(join('result', 'country_data.json'), 'w') as outfile:
+    json.dump(output_data, outfile)
