bcf185f8f39ebfc61be9f32f4b7d29183d02f5fd
satoshi <satoshi@users-iMac.local>
2017-11-04 12:59:02 -0700

    update

--- dataProcess/amazon.py
+++ dataProcess/amazon.py
@@ -1,12 +1,12 @@
 import bottlenose
 from bs4 import BeautifulSoup
-from os.path import join
+from os.path import isfile, join
 import json
 
 ACCESS_KEY = "AKIAIJWGVA4XGB6P6OQA"
 SECRET_ACCESS_KEY = "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"
 ASSOCIATE_TAG = "beavis2809-20"
- 
+
 amazon = bottlenose.Amazon(ACCESS_KEY, SECRET_ACCESS_KEY, ASSOCIATE_TAG)
 response1 = amazon.ItemSearch(Keywords="javascript", SearchIndex="KindleStore", ResponseGroup="Images, ItemAttributes, Offers")
 soup1 = BeautifulSoup(response1,"lxml")
@@ -23,7 +23,7 @@ output_data = []
 #print(soup.find_all('item')[0]('itemlinks').prettify())
 #for item in soup1.find_all('item'):
 index = 0
-for item in soup1.find_all('item'):  
+for item in soup1.find_all('item'):
   index += 1
   output_data.append({'title': item.title.text, 'image': item.mediumimage.url.text, 'url': item.url.text})
   if index == 3:
@@ -45,4 +45,3 @@ for item in soup3.find_all('item'):
 
 with open(join('result', 'amazon_data.json'), 'w') as outfile:
     json.dump(output_data, outfile)
-
